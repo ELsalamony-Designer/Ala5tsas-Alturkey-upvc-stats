2e886dd58b1a2a2e24eccbeaecfa2f4b4a69f977
ELsalamony-Designer <mahmoudelsalamony2000@gmail.com>
2025-06-23 01:48:36 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDgOby-sJuIUs8iCErmqtiMbqMuND5ZtCo",
   authDomain: "ala5tsas-alturkey-stats.firebaseapp.com",
   projectId: "ala5tsas-alturkey-stats",
-  storageBucket: "ala5tsas-alturkey-stats.firebasestorage.app",
+  storageBucket: "ala5tsas-alturkey-stats.appspot.com",
   messagingSenderId: "649675735929",
   appId: "1:649675735929:web:f90a49614895c6958ea5a8",
   measurementId: "G-X2FHKZT21J"
@@ -14,5 +15,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { db };
+export { db, storage };
